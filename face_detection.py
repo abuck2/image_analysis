c3c44e47a361b393076c292947318478e9be4399
Alexis <alexisbuckens@gmail.com>
2021-07-19 19:22:09 +0200

    MultiScale2

--- face_detection.py
+++ face_detection.py
@@ -15,3 +15,11 @@ boxes, detections = face_cascade.detectMultiScale2(gray)
 print(boxes) #4 lignes = 4 visages, x, Y, height, width
 print(detections)  # We need to find a threshold of minN (6 for ex)
 boxes, detections = face_cascade.detectMultiScale2(gray, minNeighbors=6)
+
+#Drawing boxes
+image = img
+for x, y, w, h in boxes:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255,0,0), 1)
+cv2.imshow("boxes", image)
+cv2.waitKey(2500)
+cv2.destroyAllWindows()
