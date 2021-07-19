2544aa8dd26044dc6f0b841d53788de1c6168729
Alexis <alexisbuckens@gmail.com>
2021-07-19 19:16:22 +0200

    MultiScale2

--- face_detection.py
+++ face_detection.py
@@ -4,4 +4,14 @@ import numpy as np
 img = cv2.imread('faces.jpg')
 
 #Load a pre-trained model
-face_cascade = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('model.xml')
+
+#Load the image in grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Applay grayscale to cascade class : object, #of detection
+boxes, detections = face_cascade.detectMultiScale2(gray)
+
+
+print(boxes) #4 lignes = 4 visages
+print(detections)  # We need to find a threshold of minN (6 for ex)
+boxes, detections = face_cascade.detectMultiScale2(gray, minNeighbors=6)
