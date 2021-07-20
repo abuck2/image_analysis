9b42de365fef905179a64427f69cb8d455bf3d88
Alexis <alexisbuckens@gmail.com>
2021-07-20 20:30:49 +0200

    image transforations

--- transformations.py
+++ transformations.py
@@ -31,3 +31,34 @@ def translation(image,tx,ty):
     shifted_img = cv2.warpAffine(image,M,(image.shape[1],image.shape[0]))
     display('shifted',shifted_img)
 
+
+#2. Rotation
+#2.1 With a rotation matrix
+#M = [[a, b, (1-a)Cx-bCy],[ -b, a, bCx+(1-a)Cy]]
+#Avec a = cos(t)*scale, b = sin(t)*scale
+#Cx, Cy coordinates of the center
+center = (img.shape[1]//2,img.shape[0]//2) #integer part of half
+M = cv2.getRotationMatrix2D(center,45,1) #center, angle, scale
+rotate_45 = cv2.warpAffine(img,M,(img.shape[1],img.shape[0])) #Rotation
+display('rotate 45 anti clock',rotate_45)
+
+#in a funtion
+def rotate(image,angle,scale):
+    center = (image.shape[1]//2,image.shape[0]//2)
+    M = cv2.getRotationMatrix2D(center,angle,scale)
+    rotate_img = cv2.warpAffine(image,M,(image.shape[1],image.shape[0]))
+
+    display('rotate',rotate_img)
+
+
+#3. resize
+img_resize = cv2.resize(img,(300,300),interpolation=cv2.INTER_AREA) 
+#check the interpolations techniques inter_cubic is better for large images but slow 
+
+#4. flipping
+flip_img = cv2.flip(img,0) # 1 = h,-1 = v,0 = both
+
+#5. cropping
+crop = img[100:400,100:400]
+
+
